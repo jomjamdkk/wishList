cdd0995aa9816fdb11704bb6b7be0ece35385a57
jomjamdkk <jomjamdkk@gmail.com>
2022-02-01 00:01:53 +0700

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace wishList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            int day = int.Parse(txtPrice.Text)/(int.Parse(txtIncome.Text)-int.Parse(txtExpeneses.Text));
+            txtDay.Text = day.ToString();
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            txtDay.Text = "";
+            txtIncome.Text = "";
+            txtExpeneses.Text = "";
+            txtPrice.Text = "";
+        }
+    }
+}
